fa0906547cd5bf3ef516aff7a8fe20b39c9dca92
GitHub <arif.working@gmail.com>
2013-02-18 21:36:49 +0700

    v1.0 apache2 config to also listen to port 81 and its vhost file, iptables to redirect to transparent proxy and to bypass DNS request and non-http to internet

---
+++ init/firewall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# accept DNS requests and other than port 80 (http)
+iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+# redirect port 80 to the second instance of squid (proxy2) that will give default site
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to 192.168.40.111:3300
