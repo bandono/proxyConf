a0507570e1b22a305d5e1c2439bbf2f9bdcae77e
Arif Kusbandono <arif.working@gmail.com>
2013-05-03 10:02:14 +0700

    v1.1 change firewall.sh to have start/stop argument option

--- init/firewall.sh
+++ init/firewall.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-# flush existing rules to be replaced
-iptables --flush
-iptables --table nat --flush
-iptables --delete-chain
-iptables --table nat --delete-chain
+function init_nat () {
+	# flush existing rules to be replaced
+	iptables --flush
+	iptables --table nat --flush
+	iptables --delete-chain
+	iptables --table nat --delete-chain
 
-# kernel set to allow forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
+	# kernel unset no forwarding
+	echo 0 > /proc/sys/net/ipv4/ip_forward
+}
 
-# accept DNS requests and other than port 80 (http)
-iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+function redirect_nat () {
+	# kernel set to allow forwarding
+	echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# redirect port 80 to the second instance of squid (proxy2) that will give default site
-iptables -t nat -A PREROUTING -i eth0 -p tcp ! -d 192.168.40.111 --dport 80 -j DNAT --to 192.168.40.111:3300
+	# accept DNS requests and other than port 80 (http)
+	iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+	iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+	iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 
-# exception for the above rule to handle request to proxy.pac that is not redirected to port 3300
-iptables -t nat -A POSTROUTING -p tcp -d 192.168.40.111 --dport 80 -j ACCEPT
+	# redirect port 80 to the second instance of squid (proxy2.some-company.net) that will give default site
+	iptables -t nat -A PREROUTING -i eth0 -p tcp ! -d 192.168.40.111 --dport 80 -j DNAT --to 192.168.40.111:3300
+
+	# exception for the nat rule after (below) to handle request to proxy.pac that is not redirected to port 3300
+	iptables -t nat -A POSTROUTING -p tcp -d 192.168.40.111 --dport 80 -j ACCEPT
+}
+
+case "$1" in
+ 
+start)
+	init_nat
+	redirect_nat
+	;;
+stop)
+	init_nat
+	;;
+*)
+	echo "Usage: $0 {start|stop}"
+	;;
+esac
+exit 0
