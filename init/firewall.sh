9a7e7a816605ca8de8e07126c6def5b1875eb7f3
Arif Kusbandono <arif.working@gmail.com>
2013-05-06 18:32:17 +0700

    v1.1 specify only one interface (eth0) in firewall.sh

--- init/firewall.sh
+++ init/firewall.sh
@@ -16,8 +16,8 @@ function redirect_nat () {
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 
 	# accept DNS requests and other than port 80 (http)
-	iptables -A FORWARD -i eth0 -o eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
-	iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+	iptables -A FORWARD -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+	iptables -A FORWARD -o eth0 -j ACCEPT
 	iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 
 	# redirect port 80 to the second instance of squid (proxy2.some-company.net) that will give default site
